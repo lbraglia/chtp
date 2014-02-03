5f996ed1b3ee54de392ae377eb2e353fca363656
Luca Braglia <l@np350v5c>
2014-02-03 01:20:38 +0100

    bla bla bla

--- main/04_26.c
+++ main/04_26.c
@@ -4,15 +4,18 @@ int main(void)
 {
    double pi = 0, x;
    int step = 0;
-   printf("%4s  %11s\n", "Step" , "Value of Pi");	
-   for(x=1; x>0; x+=2)  {	
+
+   /* header */
+   printf("Step\tValue of Pi\n");	
+
+   for(x=1; x<=1000001; x+=2)  {	
       if( (step%2) != 0) {
 	 pi -= (4/x);
       } else {
 	 pi += (4/x);
       }
 
-      printf("%-4d  %-11f\n", step, pi);
+      printf("%-4d\t%-11f\n", step, pi);
       step++;
    }
    return 0;
