261c170859ac79d8839e4faec3bb6bd6ee05d61b
lbraglia <lbraglia@gmail.com>
2015-01-04 23:39:29 +0100

    fixed like 03_33.c

--- src/03_34.c
+++ src/03_34.c
@@ -2,60 +2,57 @@
 
 int main()
 {
-
     int side, curcol, currow = 1;
+    int side_min = 1;
+    int side_max = 20;
 
-    printf("Insert the (integer) square side: ");
+    printf("Insert the (integer between %d and %d) square side : ",
+	   side_min, side_max);
     scanf("%d", &side);
 
+    if (side < side_min)
+	printf("Side must be > %d\n", side_min);
+    else if (side <= side_max) {
 
-    while (currow <= side) {
-
-
-	if (currow == 1) {	/* If the current row is the
-				   first row ... */
-	    curcol = 1;
-	    while (curcol <= side) {	/*... go fill it */
-		printf("*");
-		curcol++;
-	    }
-	    printf("\n");
-	    currow++;
-	}
-
-
-
-	else if (currow == side) {	/* The same if it's the last
-					   one */
-	    curcol = 1;
-	    while (curcol <= side) {
-		printf("*");
-		curcol++;
-	    }
-	    printf("\n");
-	    currow++;
-	}
-
-	else {			/* in every other row fill
-				   only first and last
-				   column */
-	    curcol = 1;
-	    while (curcol <= side) {
-		if (curcol == 1)
+	while (currow <= side) {
+	    /* If the current row is the first row ... */
+	    if (currow == 1) {	
+		curcol = 1;
+		while (curcol <= side) {	/*... go fill it */
 		    printf("*");
-		else if (curcol == side)
-		    printf("*");
-		else
-		    printf(" ");
-
-		curcol++;
+		    curcol++;
+		}
+		printf("\n");
+		currow++;
 	    }
-	    printf("\n");
-	    currow++;
+	    /* The same if it's the last one */
+	    else if (currow == side) {	
+		curcol = 1;
+		while (curcol <= side) {
+		    printf("*");
+		    curcol++;
+		}
+		printf("\n");
+		currow++;
+	    }
+	    else { /* in every other row fill only first and last column */
+		curcol = 1;
+		while (curcol <= side) {
+		    if (curcol == 1)
+			printf("*");
+		    else if (curcol == side)
+			printf("*");
+		    else
+			printf(" ");
+		    curcol++;
+		}
+		printf("\n");
+		currow++;
+	    }
 	}
 
-    }
+    } else
+	printf("side must be <= %d\n", side_max);
 
     return 0;
-
 }
