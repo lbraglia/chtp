30fd2a0b406de3304124949bbd6af2f4bbbd5ca8
lbraglia <lbraglia@gmail.com>
2015-01-05 15:15:14 +0100

    fixed 06_20

--- src/06_20.c
+++ src/06_20.c
@@ -1,50 +1,54 @@
-/* Fig. 5.10: fig05_10.c
-   Craps */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 enum Status { CONTINUE, WON, LOST };
 int roll(void);
-int craps(int win[], int lose[]);
+void craps(int win[], int lose[]);
+
 int main(void)
 {/*************/
-    srand(time(NULL));
+
+    int i;
+    int time_a, time_b;
+    int winning_freq[21] = { 0 };
+    int losing_freq[21] = { 0 };
+    int wons = 0;
     long repetitions;
+
+    srand(time(NULL));
+
     for (repetitions = 100; repetitions <= 100000; repetitions *= 10) {
-	int i;
-	int time_a, time_b;
-	int winning_freq[21] = { 0 };
-	int losing_freq[21] = { 0 };
-	int wons = 0;
 
-	/* execution & timing */
-	time_a = time(NULL);	/* start time */
-	for (i = 1; i <= repetitions; i++)
-	    craps(winning_freq, losing_freq);
-	time_b = time(NULL);	/* stop time */
-	for (i = 0; i <= 20; i++)	/* counting for winning game */
-	    wons += winning_freq[i];
+       /* execution & timing */
+       time_a = time(NULL);	/* start time */
+       for (i = 1; i <= repetitions; i++)
+	  craps(winning_freq, losing_freq);
+       time_b = time(NULL);	/* stop time */
+       for (i = 0; i <= 20; i++)	/* counting for winning game */
+	  wons += winning_freq[i];
 
-	/*printing results */
-	printf("Number of repetitions: %ld\n", repetitions);
-	printf("Time elapsed: %d sec.\n", time_b - time_a);
+       /*printing results */
+       printf("Number of repetitions: %ld\n", repetitions);
+       printf("Time elapsed: %d sec.\n", time_b - time_a);
 
-	/* win & lost vectors */
-	printf("try\twon\tlost \n");
-	for (i = 0; i <= 19; i++)
+       /* win & lost vectors */
+       printf("try\twon\tlost \n");
+       for (i = 0; i <= 19; i++) {
+	  printf("%3d\t%6d\t%6d \n",
+		 i + 1,
+		 winning_freq[i],
+		 losing_freq[i]);
+       }
+       printf("21+\t%6d\t%6d \n ", winning_freq[20], losing_freq[20]);
 
-	    /* fixme, I'm badly formatted */
-	    printf("%3d\t%6d\t%6d \n", i + 1, winning_freq[i],
-		   losing_freq[i]);
-	printf("21+\t%6d\t%6d \n ", winning_freq[20], losing_freq[20]);
-
-	/* winning probability */
-	printf("Won games: %.2f%\n", ((float) wons) / repetitions * 100);
-	printf("\n\n");
+       /* winning probability */
+       printf("Won games: %.2f%%\n", ((float) wons) / repetitions * 100);
+       printf("\n\n");
     } return 0;
 }
 
-int craps(int win[], int lose[])
+void craps(int win[], int lose[])
 {/************/
     int sum = 0;
     int myPoint = 0;
@@ -85,13 +89,9 @@ int craps(int win[], int lose[])
 
     /* ending  */
     if (status == WON) {
-
 	/* printf( "Player wins\n" ); */
 	++win[try - 1];
-    }
-
-    else {
-
+    } else {
 	/* printf( "Player loses\n" ); */
 	++lose[try - 1];
     }
