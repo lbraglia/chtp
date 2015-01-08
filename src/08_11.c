ec39767271226db572544e0053d54ae60caa5b69
lbraglia <lbraglia@gmail.com>
2015-01-08 13:43:30 +0100

    08_11

--- src/08_11.c
+++ src/08_11.c
@@ -1,6 +1,7 @@
+#include <ctype.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define DIM 5
@@ -9,55 +10,55 @@
 #define NOFSENTENCES 20
 #define BUFFERLEN 80
 
+int dice(int faces);
+
 
 int main(void)
 {
-    int lil_cntr;		/* a counter */
-    int sents_cnt;
+    int sents_cnt;		/* sentence counter */
+    int wrd_cntr;		/* word counter inside sentence */
 
     char *article[DIM] = { "the", "a", "one", "some", "any" };
     char *noun[DIM] = { "boy", "girl", "dog", "town", "car" };
     char *verb[DIM] = { "drove", "jumped", "ran", "walked", "skipped" };
     char *preposition[DIM] = { "to", "from", "over", "under", "on" };
-
-    char **words_index[WI_DIM] = { article, noun, verb, preposition };
-
-    char *sentences[NOFSENTENCES];	/* our final product */
-    char buffer[BUFFERLEN];
-
+    char buffer[BUFFERLEN] = {'\0'};
     /* to have a sentence we use in sequence article, noun, verb,
        preposition, article and noun: under here you find that
        sequence in terms of words_index indexes */
     int word_sequence[WS_DIM] = { 0, 1, 2, 3, 0, 1 };
+    char **words_index[WI_DIM];
 
+    words_index[0] = article;
+    words_index[1] = noun;
+    words_index[2] = verb;
+    words_index[3] = preposition;
 
+    srand(time(NULL));
+
+    /* sentence counter */
     for (sents_cnt = 1; sents_cnt <= NOFSENTENCES; ++sents_cnt) {
 
-
-
 	/* sentence composition */
-	for (lil_cntr = 0; lil_cntr < WS_DIM; ++lil_cntr) {
+	for (wrd_cntr = 0; wrd_cntr < WS_DIM; ++wrd_cntr) {
 
 	    /* put the random string in the buffer */
 	    strcat(buffer,
-		   *(words_index[word_sequence[lil_cntr]] + dice(DIM) - 1)
+		   *(words_index[word_sequence[wrd_cntr]] + dice(DIM) - 1)
 		);
 
 	    /* add the white space or the period */
-	    if (lil_cntr < WS_DIM - 1)
+	    if (wrd_cntr < WS_DIM - 1)
 		strcat(buffer, " ");
-	    else if (lil_cntr == WS_DIM - 1)
-		strcat(buffer, ".");
+	    else if (wrd_cntr == WS_DIM - 1)
+		strcat(buffer, ".\n");
 	    /* and uppercase the first letter */
-
-
+	    buffer[0] = toupper(buffer[0]);
 	}
 
-	/* copy the buffer in sentences and clear the buffer */
-	strcpy(sentences[sents_cnt - 1], buffer);
+	/* print and clean */
+	printf("%s", buffer);
 	buffer[0] = '\0';
-
-
     }
 
     return 0;
@@ -66,11 +67,5 @@ int main(void)
 
 int dice(int faces)
 {
-    static i = 1;
-    if (i) {
-	srand(time(NULL));
-	i--;
-    }
-
     return (1 + rand() % faces);
 }
