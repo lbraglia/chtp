4a527642ca19bc7503c7879606bd8a12abcb3964
lbraglia <lbraglia@gmail.com>
2015-01-08 14:09:31 +0100

    08_15

---
+++ src/08_15.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+void printReversed(char string[]);
+
+int main(void)
+{
+   char string[] = "String to be reversed";
+
+   printReversed(string);
+      
+   return 0;
+}
+
+void printReversed(char string[]){
+
+   char * tokenPtr = strtok(string, " ");
+   char * tokens[1000];  
+   int i = 0;
+   
+   while( tokenPtr != NULL){
+      tokens[i] = tokenPtr;
+      tokenPtr = strtok(NULL, " ");      
+      i++;
+   }
+
+   for(--i; i >= 0; --i)
+      printf("%s ", tokens[i]);
+   printf("\n");
+   
+}
