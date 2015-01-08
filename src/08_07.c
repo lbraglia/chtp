f02caab41c4a1322a125b2316d5985672d828805
lbraglia <lbraglia@gmail.com>
2015-01-08 10:14:52 +0100

    08_07

--- src/08_07.c
+++ src/08_07.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-#define MAXLEN 20
+#include <stdlib.h>
 
-int main(void)
+#define SIZE 20
+
+int main()
 {
-    char a[MAXLEN], b[MAXLEN], c[MAXLEN], d[MAXLEN];
-
-
-    gets(a);
-    gets(b);
-    gets(c);
-    gets(d);
-
-    printf("Sum is %d.\n", atoi(a) + atoi(b) + atoi(c) + atoi(d)
-	);
-
+    int i1, i2, i3, i4;
+    char s1[SIZE], s2[SIZE], s3[SIZE], s4[SIZE];
+    printf("Enter four strings representing integers:\n");
+    gets(s1);
+    gets(s2);
+    gets(s3);
+    gets(s4);
+    i1 = atoi(s1);
+    i2 = atoi(s2);
+    i3 = atoi(s3);
+    i4 = atoi(s4);
+    printf("The sum of the four integers is: %d\n", i1 + i2 + i3 + i4);
     return 0;
 }
