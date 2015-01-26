c44261eddb61c3a6028ab62f43364518f0b50b40
lbraglia <lbraglia@gmail.com>
2015-01-26 21:04:46 +0100

    13_10.c

---
+++ src/13_10.c
@@ -0,0 +1,16 @@
+/* 
+   13_10: SUMARRAY
+ */
+#include <stdio.h>
+
+#define SUMARRAY(array, nelem) \
+    while(i < nelem){ sum += array[i]; i++;} 
+#define DIM 3
+
+int main (void){
+    int i = 0, sum = 0;
+    int array[DIM] = {1, 2, 3};
+    SUMARRAY(array, DIM);
+    printf("Sum: %d\n", sum);
+    return 0;
+}
