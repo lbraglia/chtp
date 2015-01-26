db6db76eaed875955dfb0d198256b66db83f0766
lbraglia <lbraglia@gmail.com>
2015-01-26 19:20:06 +0100

    13_04

---
+++ src/13_04.c
@@ -0,0 +1,21 @@
+/* 
+   13_04: sphere volume with macro
+ */
+#include <stdio.h>
+
+#define PI 3.14159
+#define SPHERE_VOLUME(r) ( ( 4.0 / 3 ) * PI * (r) * (r) * (r)  )
+
+int main(void){
+
+    int r = 0;
+
+    printf("%6s%10s\n", "Radius", "Volume");
+    for(r = 1; r <= 10; r++){
+	printf("%6d%10.2f\n", r, SPHERE_VOLUME(r));
+    }
+    
+    return 0;
+}
+
+
