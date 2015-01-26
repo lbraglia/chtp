6b0f0db456c8e6937a019ca5a6a9a7fd8c0960c1
lbraglia <lbraglia@gmail.com>
2015-01-26 21:04:38 +0100

    13_09.c

---
+++ src/13_09.c
@@ -0,0 +1,16 @@
+/* 
+   13_09: PRINTARRAY
+ */
+#include <stdio.h>
+
+#define PRINTARRAY(array, nelem) \
+    while(i < nelem){ printf("%d ",array[i]); i++;} 
+#define DIM 3
+
+int main (void){
+    int i = 0;
+    int array[DIM] = {1, 2, 3};
+    PRINTARRAY(array, DIM);
+    printf("\n");
+    return 0;
+}
